82966b341439aafde167feb5410bce4c64222f47
BeboNewEra <abanoubsamy2341@gmail.com>
2024-01-19 18:36:45 +0200

    file 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this file makes a new file with some desired features
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
