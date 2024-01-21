0db29ec284990ef6e1b80d03b51f853440578491
BeboNewEra <abanoubsamy2341@gmail.com>
2024-01-21 20:39:45 +0200

    file advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# file for the task 
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
