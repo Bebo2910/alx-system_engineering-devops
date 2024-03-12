c211659d9bfbaa6f7aea0c6f384dabf9ed9a8346
BeboNewEra <abanoubsamy2341@gmail.com>
2024-03-12 15:06:22 +0200

    file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
+"""
+Module Docs
+"""
 import requests
-"""Model of finding out the number of subs"""
+
 
 def number_of_subscribers(subreddit):
-    # Define the URL for the subreddit's information using the Reddit API
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set a custom User-Agent to avoid being blocked by Reddit for too many requests
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
-    # Make the GET request
-    response = requests.get(url, headers=headers)
-    
-    # Check if the request was successful
+    """
+    Function Docs
+    """
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
+                            headers=header,
+                            allow_redirects=False
+                            )
     if response.status_code == 200:
-        data = response.json()
-        # Extract the subscriber count and return it
-        return data['data']['subscribers']
-    else:
-        # If the subreddit is invalid or any other error occurs, return 0
-        return 0
-
+        return response.json()['data']['subscribers']
+    return 0
