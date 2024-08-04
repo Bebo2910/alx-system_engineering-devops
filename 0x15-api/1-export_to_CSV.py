bff3f4218b21af0398f58725a61b947c9e99b4fd
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-04 20:36:49 +0300

    Task #2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module documented"""
+import csv
+import requests
+import json
+from sys import argv
+"""Module documented"""
+
+
+def main():
+    """Moduel documented"""
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{url}/users/{user_id}"
+    todos_url = f"{url}/todos?userId={user_id}"
+    filename = f"{user_id}.csv"
+
+    # Fetch user data
+    resp1 = requests.get(user_url)
+    resp1.raise_for_status()
+    user_data = resp1.json()
+
+    # Fetch todos data
+    resp2 = requests.get(todos_url)
+    resp2.raise_for_status() 
+    todos = resp2.json()
+
+    data = []
+    for task in todos:
+        data.append([user_id, user_data["name"], task["completed"], task["title"]])
+
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
+
+if __name__ == '__main__':
+    """Module Docuemented"""
+    main()
