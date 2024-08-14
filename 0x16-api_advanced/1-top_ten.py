289f102cf1ad40269857c883d38a78e9884b7ee8
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-14 14:10:08 +0300

    editedg

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,8 +14,8 @@ def top_ten(subreddit):
         'Accept': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
-    response = requests.get('{}/r/{}/.json?sort={}&limit={}'.format(
-        url, subreddit, 'top', 10),
+    response = requests.get('{}/r/{}/hot.json?limit={}'.format(
+        url, subreddit, 10),
         headers=header,
         allow_redirects=False)
     if response.status_code == 200:
