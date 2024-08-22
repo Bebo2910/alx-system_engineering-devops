24fd4e1b987338f6950827dad7d0192b111c7346
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-08-22 15:36:55 +0300

    making debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,30 +1,12 @@
-class nginx_setup {
-  package { 'nginx':
-    ensure => installed, # Ensure Nginx is installed
-  }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('nginx/nginx.conf.erb'), # Use an ERB template to populate the configuration
-    require => Package['nginx'], # This configuration requires that the Nginx package is installed
-    notify  => Service['nginx'], # Notify the Nginx service if the file changes
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    require   => File['/etc/nginx/nginx.conf'], # This service requires the Nginx configuration file
-  }
+# sets file limite for nginx to 4096
+# Usage example: puppet apply 0-the_sky_is_the_limit_not.pp
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-# Example content of nginx/nginx.conf.erb (you need to create this template file)
-# worker_processes auto;
-# worker_connections 1024;
-# ... Add other configurations as needed ...
-
-# To apply the manifest:
-# puppet apply 0-the_sky_is_the_limit_not.pp
-
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
